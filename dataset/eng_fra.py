e75eda9535d79c72edc22a3f4a9a778756de7262
wanglijun <juns1984@qq.com>
2018-06-04 20:17:42 +0800

    init

--- dataset/eng_fra.py
+++ dataset/eng_fra.py
@@ -21,12 +21,14 @@ def _read_sentence_pair(max_seq_len, padding):
             line = line.strip(' \n')
             try:
                 eng, fra = re.split('[\t]', line)
-                eng_tokens = tuple(islice(
+                eng_tokens = list(islice(
                     chain(tokenizer(eng, use_lib="spacy", lang="en"), padding),
                     0, max_seq_len))
-                fra_tokens = tuple(islice(
+                fra_tokens = list(islice(
                     chain(tokenizer(fra, use_lib="spacy", lang="fr"), padding),
                     0, max_seq_len))
+                eng_tokens.append(EOS)
+                fra_tokens.append(EOS)
                 yield eng_tokens, fra_tokens
             except ValueError as e:
                 print(line, e)
@@ -39,13 +41,12 @@ def _tokens(index, pair_seq):
 
 def load_dataset(min_count, max_seq_len):
     pair_seq = list(islice(_read_sentence_pair(max_seq_len=max_seq_len, padding=PAD), 0, 10))
-    en_vocab = Vocab(words=_tokens(0, pair_seq))
-    fr_vocab = Vocab(words=_tokens(1, pair_seq))
-    en_lang = Lang(words=en_vocab.shrink(min_count=min_count), reserved_tokens=(SOS, EOS, PAD), nil_token=UNK)
-    fr_lang = Lang(words=fr_vocab.shrink(min_count=min_count), reserved_tokens=(SOS, EOS, PAD), nil_token=UNK)
+    en_vocab = Vocab(words=_tokens(0, pair_seq), min_count=min_count)
+    fr_vocab = Vocab(words=_tokens(1, pair_seq), min_count=min_count)
+    en_lang = Lang(words=en_vocab, reserved_tokens=(SOS, EOS, PAD), nil_token=UNK)
+    fr_lang = Lang(words=fr_vocab, reserved_tokens=(SOS, EOS, PAD), nil_token=UNK)
     new_pairs = []
     for eng, fra in pair_seq:
         new_pairs.append((en_lang.to_indices(eng), fr_lang.to_indices(fra)))
 
     return en_lang, fr_lang, train_test_split(new_pairs, test_size=0.2, shuffle=True)
-
--- model/encoder.py
+++ model/encoder.py
@@ -1,1 +1,31 @@
 import torch
+import torch.nn as nn
+from conf import run_device
+
+
+class Encoder(nn.Module):
+    def __init__(self, lang, embedding_dim=100, hidden_size=512):
+        super(Encoder, self).__init__()
+        self.lang = lang
+        self.num_layers = 2
+        self.hidden_size = hidden_size
+        self.bidirectional = True
+        self.embedding = nn.Embedding(num_embeddings=lang.vacab_size, embedding_dim=embedding_dim)
+        self.rnn = nn.GRU(
+            input_size=embedding_dim,
+            dropout=0.5,
+            hidden_size=hidden_size,
+            num_layers=self.num_layers,
+            bidirectional=self.bidirectional
+        )
+
+    def begin_state(self):
+        bidirect = 2 if self.bidirectional else 1
+        return torch.zeros(self.num_layers * bidirect, 1, self.hidden_size, dtype=torch.float, device=run_device())
+
+    def forward(self, words, state):
+        words = words.to(run_device())
+        input_len = len(words)
+        words = self.embedding(words).view(input_len, 1, -1)
+        output, state = self.run(words, state)
+        return output, state
